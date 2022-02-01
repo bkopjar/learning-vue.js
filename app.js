6c82cb24b047da3864d4d8e04a850471c8a330f6
bkopjar <bernardo.kopjar@gmail.com>
2022-02-01 21:03:32 +0100

    Added random user generator button and functionality

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const app = Vue.createApp({
             lastName: "Smith",
             email: "john@gmail.com",
             gender: "male",
-            image: "https://randomuser.me/api/portraits/men/75.jpg",
+            image: "https://randomuser.me/api/portraits/men/3.jpg",
         }
+    },
+
+    methods: {
+        async getUser() {
+            const res = await fetch("https://randomuser.me/api")
+            const { results } = await res.json()
+            //console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.image = results[0].picture.large
+        },
     },
 })
 
